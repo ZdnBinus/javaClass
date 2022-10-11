53722bf6527986c69cead0a4e5fd440264af05ad
ZdnBinus <33022238+ZdnBinus@users.noreply.github.com>
2022-10-11 09:38:49 +0700

    Pertemuan 3 111022

---
+++ Main.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * variable switch
+		String x = "air";
+		String y = "teh";
+		String temp;
+		
+		System.out.println("x: "+ x);
+		System.out.println("y: "+ y);
+		System.out.println("");
+		
+		temp = x;
+		x = y;
+		y = temp;
+		
+		System.out.println("x: "+ x);
+		System.out.println("y: "+ y);*/
+		
+		
+		//scanner input user data
+		//Scanner scan = new Scanner(System.in);
+		
+		//int x = scan.nextInt();
+		
+		//System.out.println("x: "+x);
+		
+		//System.out.println("Nama: ?");
+		//String nama = scan.next();
+		
+		
+		//System.out.println("nama: "+nama);
+		
+		
+		//type casting
+//		int x = 9;
+//		double y = x;
+//		
+//		System.out.println(x);
+//		System.out.println(y);
+		
+		//operator
+//		int x = 10;
+//		int y = 3;
+//		
+//		System.out.println(x+y);
+//		System.out.println(x-y);
+//		System.out.println(x*y);
+//		System.out.println(x/y);
+//		
+//		System.out.println(x%y);
+//		
+//		//increment nambah nilai variabel 1
+//		x++;
+//		System.out.println(x);
+//		//dicrement kurang niali var 1
+//		x--;
+//		System.out.println(x);
+		
+		int x = 8;
+		int y = 2;
+		
+		if (x > y) {
+			System.out.println("x lebih besar dari y");
+			
+		}else {
+			System.out.println("ganjil");
+			
+		}
+	
+		
+		
+		
+		
+
+	}
+
+}
